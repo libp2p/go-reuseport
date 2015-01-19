dbf0f586c232dc737f97d16ad0a126bfb44ddcac
Juan Batiz-Benet <juan@benet.ai>
2015-01-18 20:15:58 -0800

    Dial() func

--- interface.go
+++ interface.go
@@ -3,6 +3,8 @@ package reuseport
 import (
 	"errors"
 	"net"
+
+	resolve "github.com/jbenet/go-net-resolve-addr"
 )
 
 // ErrUnsuportedProtocol signals that the protocol is not currently
@@ -17,6 +19,23 @@ var ErrReuseFailed = errors.New("protocol not yet supported")
 // with SO_REUSEPORT and SO_REUSEADDR option set.
 func Listen(network, address string) (net.Listener, error) {
 	return listen(network, address)
+}
+
+// Dial dials the given network and address. see net.Dialer.Dial
+// Returns a net.Conn created from a file discriptor for a socket
+// with SO_REUSEPORT and SO_REUSEADDR option set.
+func Dial(network, laddr, raddr string) (net.Conn, error) {
+
+	var d Dialer
+	if laddr != "" {
+		netladdr, err := resolve.ResolveAddr("dial", network, laddr)
+		if err != nil {
+			return nil, err
+		}
+		d.D.LocalAddr = netladdr
+	}
+
+	return dial(d.D, network, raddr)
 }
 
 // Dialer is used to specify the Dial options, much like net.Dialer.
